db53aee1029c28e2f93574ffdd144e52a6a50ccd
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-09-23 15:10:14 +0200

    Add logo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <ol>
     <li><a href="hello-world/">Hello world</a></li>
     <li><a href="oversized-canvas/">Oversized canvas</a></li>
+    <li><a href="logo/">Logo</a></li>
     <li><a href="landing-page-bubbles/">Landing page with bubbles</a></li>
     <li><a href="landing-page-halo/">Landing page with a halo</a></li>
     <li><a href="slider/">Slider component</a></li>
