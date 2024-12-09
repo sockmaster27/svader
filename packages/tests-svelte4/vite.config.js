e896af95c0757c78631e41827c439e2b53c4b856
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-12-09 13:49:04 +0100

    Fix type mistmatch in vite config

--- packages/tests-svelte4/vite.config.js
+++ packages/tests-svelte4/vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    // @ts-ignore:
+    // Because we're using vite-plugin-svelte 3 and vite 6,
+    // these types are not entriely compatible, but in practice it works.
     plugins: [sveltekit()],
     preview: {
         port: 4173,
