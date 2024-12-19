f3be49eadef8caab546cbb1a5be5aa57d2a8355d
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-12-19 14:46:10 +0100

    Disable retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,8 +11,6 @@ export default defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : 4,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
