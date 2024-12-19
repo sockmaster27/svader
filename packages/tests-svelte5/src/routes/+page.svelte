5e81cb96c8e50cfbfdbbd59e66cb4dcb441adb68
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-12-19 11:01:50 +0100

    Remove link to logo page

--- packages/tests-svelte5/src/routes/+page.svelte
+++ packages/tests-svelte5/src/routes/+page.svelte
@@ -2,7 +2,6 @@
     <li><a href="hello-world/">Hello world</a></li>
     <li><a href="remount/">Remounting canvas</a></li>
     <li><a href="oversized-canvas/">Oversized canvas</a></li>
-    <li><a href="logo/">Logo</a></li>
     <li><a href="landing-page-bubbles/">Landing page with bubbles</a></li>
     <li><a href="landing-page-halo/">Landing page with a halo</a></li>
     <li><a href="slider/">Slider component</a></li>
